e6b3b92f4adbf4dd61693511831f3efeb0228261
Jamie Wright <jamie@brilliantfantastic.com>
2017-06-10 13:34:29 -0400

    Change the redirect to point to the correct place

--- config/prod.exs
+++ config/prod.exs
@@ -2,7 +2,7 @@ use Mix.Config
 
 config :gotv, Gotv.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [scheme: "https", host: "gotv-staging.herokuapp.com", port: 443],
+  url: [scheme: "http", host: "www.toledogo.tv", port: 80],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/manifest.json",
   secret_key_base: System.get_env("SECRET_KEY_BASE")
