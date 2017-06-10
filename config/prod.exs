4b5d08bb058855bc96fdaac850aca8b42c9713ff
Jamie Wright <jamie@brilliantfantastic.com>
2017-06-10 13:47:08 -0400

    Do not force ssl

--- config/prod.exs
+++ config/prod.exs
@@ -3,7 +3,6 @@ use Mix.Config
 config :gotv, Gotv.Endpoint,
   http: [port: {:system, "PORT"}],
   url: [scheme: "http", host: "www.toledogo.tv", port: 80],
-  force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/manifest.json",
   secret_key_base: System.get_env("SECRET_KEY_BASE")
 
